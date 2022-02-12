68ddaa7d8d4a1c536e39e261194936cb6e123cb3
Roman Neyman <neyman.roma@gmail.com>
2022-02-12 22:59:20 +0200

    comparison tasks

---
+++ comparison/script.js
@@ -0,0 +1,7 @@
+5 > 4 // true
+"ананас" > "яблоко" // false, "а" > "я"
+"2" > "12" // true, "2" > "1"
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
